034f430391b02d6ac685904888537dddd3f8e9db
George Erickson <george55@mit.edu>
2012-04-11 23:22:51 -0400

    initializing project

---
+++ watch.rb
@@ -0,0 +1,3 @@
+atch('src/jade/.*.jade') { |md| system "jade src/jade/index.jade --out build"}
+watch('src/less/.*.less') { |md| system "lessc src/less/base.less build/base.css; echo #{md[0]}"}
+watch('src/coffee/.*.coffee') { |md| system "coffee -pb src/coffee/main.coffee > build/main.js; echo #{md[0]}"}
